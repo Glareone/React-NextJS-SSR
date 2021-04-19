a971e480658ab24c13ed2e434728b489f8201dd3
Glareone <enlarius@gmail.com>
2021-04-19 14:29:22 +0300

    Worked example

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
-export default () => (
-    <p1>Hello</p1>
-);
+import styles from './index.module.scss';
+import Card from './Card';
 
+export default () => (
+    <div className={styles.App}>
+        <header className={styles.AppHeader}>
+            <img src="/static/logo.png"
+                className={styles.staticLogo} alt="logo"
+            />
+        </header>
+        <div className={styles.Grid}>
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    </div>
+);
